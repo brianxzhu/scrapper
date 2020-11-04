ba5fb600520b64e52595e3ecce3ac40c51384af3
Peter Chau <peterchau25@gmail.com>
2020-11-04 13:12:10 -0800

    Add files via upload

---
+++ Car_make_tool.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[29]:
+
+
+car_make = {
+  "Abarth": "Abarth",
+  "Alfa Romeo":  "Alfa Romeo",
+  "Aston Martin": "Aston Martin",
+  "Audi": "Audi",
+  "Bentley": "Bentley",
+  "BMW": "BMW",
+  "Bugatti": "Bugatti",
+  "Cadillac": "Cadillac",
+  "Chevrolet": "Chevrolet",
+  "Chrysler": "Chrysler",
+  "Chevy": "Chevy",  
+  "Citroën": "Citroën",
+  "Dacia": "Dacia",
+  "Daewoo": "Daewoo",
+  "Daihatsu": "Daihatsu",
+  "Dodge": "Dodge",
+  "Ferrari": "Ferrari",
+  "Fiat": "Fiat",
+  "Fisker": "Fisker",
+  "Ford": "Ford",
+  "Honda": "Honda",
+  "Hummer": "Hummer",
+  "Hyundai": "Hyundai",
+  "Infiniti": "Infiniti",
+  "Jaguar": "Jaguar",
+  "Jeep": "Jeep",
+  "Kia": "Kia",
+  "Lamborghini": "Lamborghini",
+  "Lancia":  "Lancia",
+  "Land Rover": "Land Rover",
+  "Lexus":"Lexus",
+  "Lotus":"Lotus",
+  "Maserati": "Maserati",
+  "Maybach": "Maybach",
+  "Mazda": "Mazda",
+  "McLaren": "McLaren",
+  "Mercedes-Benz": "Mercedes-Benz",
+  "Mini": "Mini",
+  "Mitsubishi": "Mitsubishi",
+  "Morgan": "Morgan",
+  "Nissan": "Nissan",
+  "Peugeot": "Peugeot",
+  "Porsche": "Porsche",
+  "Renault": "Renault",
+  "Rolls-Royce": "Rolls-Royce",
+  "Rover": "Rover",
+  "Saab": "Saab",
+  "Seat": "Seat",
+  "Skoda": "Skoda",
+  "Smart": "Smart",
+  "Subaru":  "Subaru",
+  "Suzuki": "Suzuki",
+  "Tesla": "Tesla",
+  "Toyota": "Toyota",
+  "Volkswagen": "Volkswagen",
+  "Volvo":  "Volvo"
+}
+
+a = "1991 mitsubishi 3000gt vr4 twin turbo" # header of CL post
+
+b = a.split() # split header into list of strings
+
+print(b) # confirms list 
+
+no_int = [x for x in b if not(x.isdigit()                 
+            or x[0] == '-' and x[1:].isdigit())] #removes any digits in strings 
+
+no_int = [p.capitalize() for p in no_int] # capitalize first letter of string to match up against car make list
+
+print (no_int) # confirms list
+ 
+
+
+# In[30]:
+
+
+# checks strings in list to car make and return car make
+
+cars = [i for i in car_make]
+for i in no_int : 
+    if i in cars: 
+        print (i)
+
